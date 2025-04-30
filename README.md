0c3515e85f9c989ad7f817ee426209d872e802e6
Kausik Sivakumar <kausik@nec-labs.com>
2025-04-30 15:35:59 -0700

    Passed in the right latent from vae - stable diffusion

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Reimplementation of diffusion transformer
 1. conda create --name dit python=3.10
 2. pip3 install torch torchvision torchaudio
 3. conda install conda-forge::opencv
+4. conda install -c conda-forge diffusers
+5. pip install accelerate
--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from utils import get_sinusoidal_position_embeddings
 class ImagePatchifyer(torch.nn.Module):
     
     def __init__(self, latent_rep_div: int = 8) -> None:
+        # TODO: I need to use a pretrained autoencoder from stable diffusion and pass that as latent
         super().__init__()
         '''
         Assumes all images are of size 256 x 256
@@ -12,20 +13,15 @@ class ImagePatchifyer(torch.nn.Module):
         assert 256% latent_rep_div == 0, "We need the image dims to be divisible by patch size for effective pathification"
         patch_size = 2 # hardcoded for now, we are only working with patch_size = 2 for this reimplementation
         
-        self.latent_conv = torch.nn.Conv2d(in_channels=3, 
-                                          out_channels=4, 
-                                          kernel_size=latent_rep_div, 
-                                          stride=latent_rep_div)
-        
         self.unfold = torch.nn.Unfold(kernel_size=patch_size, stride=patch_size) 
 
         self.linear_layer = torch.nn.Linear(16, 384) # d -> 384 (size of each token embedding)
 
-    def forward(self, img: torch.Tensor) -> torch.Tensor: 
-        assert img.shape[1:] == (3, 256, 256), f"Image dimensions must be 256x256x3 in this implementation but got {img.shape[1:]}"
-        latent_rep = self.latent_conv(img) # (B, 4, 32, 32)        
+    def forward(self, latent_z: torch.Tensor) -> torch.Tensor: 
+        assert latent_z.shape[1:] == (4, 32, 32), f"Image's latent dimensions must be 4x32x32 in this implementation but got {latent_z.shape[1:]}"
+        # latent_rep = self.latent_conv(latent_z) # (B, 4, 32, 32)        
         # Now get patches of size 2
-        latent_img_patches = self.unfold(latent_rep).permute(0,2,1) # (B, 256, 16)
+        latent_img_patches = self.unfold(latent_z).permute(0,2,1) # (B, 256, 16)
         img_tokens = self.linear_layer(latent_img_patches) # (B, T , d) --> (B, T, 384)
         
         # TODO: Add position embeddings
@@ -40,11 +36,19 @@ class ImagePatchifyer(torch.nn.Module):
 if __name__ == '__main__':
     from data import ImageNetDataset
     from torch.utils.data import DataLoader
+    from diffusers.models import AutoencoderKL
+    vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae")
+    vae.eval()
+
     patchifyer = ImagePatchifyer()
     data = ImageNetDataset()
     dataloader = DataLoader(data, batch_size=1,
                         shuffle=True, num_workers=0)
     next_img_sample = next(iter(dataloader))
     img = next_img_sample["img"]
-    output = patchifyer(img)
+    with torch.no_grad():
+        # How to get latent dim
+        z = vae.encode(img).latent_dist.sample() * 0.18215
+
+    output = patchifyer(z)
     print(output.shape)
